bc4c6fe44470f0c24ae18f9c24438888c1ee3533
rockwillck <rockwill@sparkplug.page>
2024-05-28 23:16:33 -0500

    bmac

--- post.py
+++ post.py
@@ -36,6 +36,7 @@ newFile.write(f"""<!DOCTYPE html>
 	<div class="content">
 		{content}
 	</div>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="rockwill" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </body>
 </html>""")
 
