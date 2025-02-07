0c2ee231e15ea80a2b1831ec0e5cc28ff745138a
William Choi-Kim <will.choi.kim@gmail.com>
2025-02-06 20:21:52 -0600

    fixed bugs with loading + new source for feed titles to improve loading times

---
+++ scripts/feed_loader.js
@@ -0,0 +1,57 @@
+async function getData() {
+    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNzHtt1-FLZgKBvCzwbrfHiY129oKg1ecKKksXo3dsY_HRVmHz2ftWWG4jFDs0YFTPUYZGRnfQ_Hs9/pub?gid=1511671296&single=true&output=csv";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        // get spreadsheet contents
+        response.text().then(
+            (data) => {
+                let authors = data.split("\n").map(x => x.substring(x.lastIndexOf(",") + 1))
+                let remaining = data.split("\n").map(x => x.substring(0, x.lastIndexOf(",")))
+                let titles = remaining.map(x => x.substring(0, x.lastIndexOf(",")))
+                let tags = remaining.map(x => x.substring(x.lastIndexOf(",") + 1))
+                titles = titles.map(x => x.trim()).map(x => x.startsWith("\"") ? x.slice(1,) : x).map(x => x.endsWith("\"") ? x.slice(0, -1) : x)
+
+                let replaced = []
+                let tagList = []
+                for (let i = 0; i < titles.length; i++) {
+                    let title = titles[i]
+                    let aut = authors[i]
+                    let tag = tags[i]
+                    let contents = `${title.split("...")[0].trim()}...
+                                <sub>${title.split("...")[1].trim()}</sub>
+                                <div class="author">
+                                    ${aut}
+                                </div>`
+                    // place in tag category on home page
+                    aa = document.createElement("a")
+                    aa.href = `/article.html?${i}`
+                    aa.className = "story " + tag + "TAG"
+                    aa.innerHTML = contents
+                    document.getElementById(tag + " Place").parentNode.insertBefore(aa, document.getElementById(tag + " Place"))
+                    replaced.push(tag + " Place")
+                    tagList.push(tag)
+
+
+                    if (i >= titles.length - 2) {
+                        document.getElementById(`new${i - titles.length + 3}`).href = `/article.html?${i}`
+                        document.getElementById(`new${i - titles.length + 3}`).innerHTML = contents
+                    }
+                }
+
+                // remove loading placeholders
+                replaced.filter((x, i) => replaced.indexOf(x) == i).map(x => document.getElementById(x).remove())
+
+                let tagCont = document.getElementById("tags")
+                for (tag of tagList.filter((x, i) => tags.indexOf(x) == i)) {
+                    tagCont.innerHTML += `<button class="tagSelected" onclick="deactivateTag('${tag}', this)">${tag}</button>`
+                }
+            })
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getData()
--- scripts/read.js
+++ scripts/read.js
@@ -1,70 +1,32 @@
 async function getData() {
     const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNzHtt1-FLZgKBvCzwbrfHiY129oKg1ecKKksXo3dsY_HRVmHz2ftWWG4jFDs0YFTPUYZGRnfQ_Hs9/pub?gid=859842630&single=true&output=csv";
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      // get spreadsheet contents
-      response.text().then(
-        (data) => {
-            // parse spreadsheet contents
-            let articles = data.split("\n").filter(x => (!x.startsWith(","))).splice(1).join("\n").split("(::)(::)").filter(y => y != "\r")
-            
-            // serve specific article
-            if (window.location.href.includes("article")) {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        // get spreadsheet contents
+        response.text().then(
+            (data) => {
+                console.log(data)
+                // parse spreadsheet contents
+                let articles = data.split("\n").filter(x => (!x.startsWith(","))).splice(1).join("\n").split("(::)(::)").filter(y => y != "\r")
+
+                // serve specific article
                 num = parseInt(window.location.href.split("?")[1])
                 art = articles[num]
-                let title = art.split(",\"")[0].split("...").map(x => x.trim()).join(" / ")
+                let title = art.split(",\"")[0].split("...").map(x => x.trim()).map(x => x.startsWith("\"") ? x.slice(1,) : x).map(x => x.endsWith("\"") ? x.slice(0, -1) : x).join(" / ")
                 let aut = art.split(",")[art.split(",").length - 3]
                 let tag = art.split(",")[art.split(",").length - 2]
-                let text = art.slice(art.split(",\"")[0].length + 2, art.length-tag.length-aut.length-3)
+                let text = art.slice(art.split(",\"")[0].length + 2, art.length - tag.length - aut.length - 3)
+                text = text.substring(0, text.lastIndexOf("\""))
                 document.getElementById("tit").innerText = title
                 document.getElementById("aut").innerText = aut
                 document.getElementById("cont").innerText = text
-            } /* serve all articles */ else {
-                let index = 0
-                let replaced = []
-                let tags = []
-                for (let art of articles) {
-                    let title = art.split(",\"")[0]
-                    let aut = art.split(",")[art.split(",").length - 3]
-                    let tag = art.split(",")[art.split(",").length - 2]
-                    let contents = `${title.split("...")[0].trim()}...
-                                <sub>${title.split("...")[1].trim()}</sub>
-                                <div class="author">
-                                    ${aut}
-                                </div>`
-                    // place in tag category on home page
-                    aa = document.createElement("a")
-                    aa.href = `/article.html?${index}`
-                    aa.className = "story " + tag + "TAG"
-                    aa.innerHTML = contents
-                    document.getElementById(tag + " Place").parentNode.insertBefore(aa, document.getElementById(tag + " Place"))
-                    replaced.push(tag + " Place")
-                    tags.push(tag)
-    
-
-                    if (index >= articles.length - 2) {
-                        document.getElementById(`new${index-articles.length+3}`).href = `/article.html?${index}`
-                        document.getElementById(`new${index-articles.length+3}`).innerHTML = contents
-                    }
-    
-                    index++
-                }
-
-                // remove loading placeholders
-                replaced.filter((x, i) => replaced.indexOf(x) == i).map(x => document.getElementById(x).remove())
-
-                let tagCont = document.getElementById("tags")
-                for (tag of tags.filter((x, i) => tags.indexOf(x) == i)) {
-                    tagCont.innerHTML += `<button class="tagSelected" onclick="deactivateTag('${tag}', this)">${tag}</button>`
-                }
-            }
-        })
+            })
     } catch (error) {
-      console.error(error.message);
+        console.error(error.message);
     }
-  }
-  getData()
+}
+getData()
