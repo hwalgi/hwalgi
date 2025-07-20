be53be67ce4303f61dddd5ee82f1357ca680abd3
William Choi-Kim <will.choi.kim@gmail.com>
2025-07-19 22:42:17 -0500

    made headers disappear along with story sections

--- scripts/tagging.js
+++ scripts/tagging.js
@@ -2,6 +2,7 @@ function deactivateTag(ta, but) {
     let stos = document.getElementsByClassName(ta + "TAG")
     but.className = !stos[0].hidden ? "" : "tagSelected"
     for (let sto of stos) {
+        sto.previousElementSibling.hidden = !sto.hidden
         sto.hidden = !sto.hidden
     }
 }
