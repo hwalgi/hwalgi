ca0aa6755b8967ef8f5ccf9dd3f6233e6e71237e
William Choi-Kim <will.choi.kim@gmail.com>
2025-08-20 20:55:16 -0500

    fixed banner

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,18 @@
 // letter animations on load
 window.addEventListener("load", () => {
-    // document.body.innerHTML += `
-	// <div id="banner">
-	// 	Please clear your cache if Hwalgi is not loading properly.
-	// </div>
-    // `
-    // setInterval(() => {
-    //     y = window.scrollY
-    //     if (y > 200) {
-    //         document.getElementById("banner").style.top = "-500px"
-    //     } else {
-    //         document.getElementById("banner").style.top = ""
-    //     }
-    // }, 300)
+    document.body.innerHTML += `
+	<div id="banner">
+		Please clear your cache if Hwalgi is not loading properly.
+	</div>
+    `
+    setInterval(() => {
+        y = window.scrollY
+        if (y > 200) {
+            document.getElementById("banner").style.top = "-500px"
+        } else {
+            document.getElementById("banner").style.top = ""
+        }
+    }, 300)
     document.getElementById("title").innerHTML = document.getElementById("title").innerText.split("").map((x, i) => `<p class="letter" style="
 	translate: 0 1em;">${x}</p>`).join("")
     letters = document.getElementById("title").getElementsByClassName("letter")
