234e63e8d85bde83bfb68157d11f39af8f8444d4
William Choi-Kim <will.choi.kim@gmail.com>
2025-09-01 20:33:31 -0500

    fix sitemap js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -34,19 +34,17 @@ export async function onRequest(context) {
 
     const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNzHtt1-FLZgKBvCzwbrfHiY129oKg1ecKKksXo3dsY_HRVmHz2ftWWG4jFDs0YFTPUYZGRnfQ_Hs9/pub?gid=1511671296&single=true&output=csv";
     const response = await fetch(url);
-    response.text().then(
-        (data) => {
-            let remaining = data.split("\n").map(x => x.substring(0, x.lastIndexOf(",")))
-            let titles = remaining.map(x => x.substring(0, x.lastIndexOf(",")))
-            for (let title of titles) {
-                xml += `<url>
+    const data = await response.text()
+    
+    let remaining = data.split("\n").map(x => x.substring(0, x.lastIndexOf(",")))
+    let titles = remaining.map(x => x.substring(0, x.lastIndexOf(",")))
+    for (let title of titles) {
+        xml += `<url>
     <loc>https://www.hwalgi.org/article/${getSplat(title)}</loc>
     <changefreq>yearly</changefreq>
     <priority>0.9</priority>
 </url>`
-            }
-        }
-    )
+    }
 
     return new Response(xml+"\n</urlset>", {
         headers: { "content-type": "text/html; charset=UTF-8" },
