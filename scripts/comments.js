22b8971e57b32713267605d239700170f71ac26c
William Choi-Kim <will.choi.kim@gmail.com>
2025-09-01 20:51:44 -0500

    fixing comments

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,4 +1,4 @@
-function loadComments(postNum) {
+function loadComments(num) {
     document.getElementById('submissionForm').addEventListener('submit', function(event) {
         document.getElementById("commentID").value = window.location.href.split("?")[window.location.href.split("?").length - 1]
         event.preventDefault(); // Prevents the default form submission
@@ -23,15 +23,15 @@ function loadComments(postNum) {
 
     fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRBLq2bMDXDUL2lzDZK_AtEdaEb7-hxhYarWduAI0cSqQGx65xpeaC6T6cgUj8cTDdkw-pKcxeKkTjj/pub?gid=1144327213&single=true&output=csv").then(x => x.text()).then(
         x => {
+            postNum = `${num}`
             commentsRaw = x.split("\n").filter(x => x.startsWith(postNum)).map(x => x.slice(postNum.length)).filter(x => !x.startsWith(",#N/A")).map(x => [x.slice(0, x.length - ("hwalgi" + x.split(",hwalgi")[x.split(",hwalgi").length - 1]).length - 1), "hwalgi" + x.split(",hwalgi")[x.split(",hwalgi").length - 1].trim().replace(",","")])
             document.getElementById("comments").innerHTML = ""
             for (comment of commentsRaw.reverse()) {
                 commentText = comment[0]
                 commentText = commentText.startsWith("\"") ? commentText.slice(1, commentText.length - 1) : commentText
-                console.log(commentText)
                 document.getElementById("comments").innerHTML += `
                 <div class="comment">
-                    <sub>${comment[1].split("@")[0]}</sub>
+                    <small>${comment[1].split("@")[0]}</small>
                     <small class="date">${comment[1].split("@")[1]}</small>
                     <br>
                     ${commentText}
