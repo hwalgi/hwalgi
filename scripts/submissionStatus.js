5142b65cc5d4fb2e326112bad4dfb44e8c2d26ea
William Choi-Kim <will.choi.kim@gmail.com>
2025-09-07 17:50:35 -0500

    change to form status page to combat 413

---
+++ scripts/submissionStatus.js
@@ -0,0 +1,20 @@
+
+id = window.location.href.split("?")[1]
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQNzHtt1-FLZgKBvCzwbrfHiY129oKg1ecKKksXo3dsY_HRVmHz2ftWWG4jFDs0YFTPUYZGRnfQ_Hs9/pub?gid=1725594247&single=true&output=csv").then(r => r.text()).then(t => {
+    if (t.includes(id)) {
+        document.getElementById("title").innerText = "Success!"
+        document.getElementById("sub").innerText = "Your submission has been received. We'll be in touch soon!"
+    } else {
+        submissionTime = parseInt(id.split("-")[0])
+        currentTime = Date.now()
+        delta = currentTime - submissionTime
+        if (delta/(1000*60) < 6) {
+            document.getElementById("title").innerText = "Pending..."
+            document.getElementById("sub").innerText = "Come back in 5-10 minutes. You don't need to keep this page open, but keep it bookmarked so you can check if your submission goes through."
+        } else {
+            document.getElementById("title").innerText = "Oh no!"
+            document.getElementById("sub").innerText = "Your submission didn't go through. We might be having issues with our form - email us your writing at <a href=\"mailto:contact@hwalgi.org\">contact@hwalgi.org</a>."
+        }
+        
+    }
+})
--- scripts/submit.js
+++ scripts/submit.js
@@ -8,15 +8,15 @@ const formAttributes = {
     "text": "entry.1817590422"
 }
 
-function submitForm() {
+async function submitForm() {
     try {
-        submitInner();
+        await submitInner();
     } catch (error) {
         window.location.href = "/formOut_fail.html"
     }
 }
 
-function submitInner() {
+async function submitInner() {
     if (document.getElementById("agree").checked == false) {
         alert("You must agree to the terms before submitting.")
         return
@@ -25,6 +25,37 @@ function submitInner() {
     const formData = new FormData();
     for (el of [...document.getElementById("submissionForm").children].filter(e => Object.keys(formAttributes).includes(e.id))) {
         if (el.id == "text") {
+            content = el.innerHTML
+
+            content = content.replaceAll(/font-size:\s*[^;]+;/g, "")
+            content = content.replaceAll("font-family: Arial, sans-serif;", "");
+            content = content.replaceAll("color: rgb(20, 22, 23);", "");
+            content = content.replaceAll("background-color: transparent;", "");
+            content = content.replaceAll("font-variant-numeric: normal;", "");
+            content = content.replaceAll("font-variant-east-asian: normal;", "");
+            content = content.replaceAll("font-variant-alternates: normal;", "");
+            content = content.replaceAll("font-variant-position: normal;", "");
+            content = content.replaceAll("font-variant-emoji: normal;", "");
+            content = content.replaceAll("vertical-align: baseline;", "");
+            content = content.replaceAll("white-space-collapse: preserve;", "");
+            while (content.includes("  ")) {
+                content = content.replaceAll("  ", " ");
+            }
+            while (content.includes("\n\n")) {
+                content = content.replaceAll("\n\n", "\n");
+            }
+            content = content.replaceAll(/\s*id="[^"]*"/g, "")
+            content = content.replaceAll(/\s*dir="[^"]*"/g, "")
+            content = content.replaceAll("\" ", "\"")
+            content = content.replaceAll(" \"", "\"")
+            content = content.replaceAll("style=\"\"", "")
+            content = content.replaceAll("line-height:2.4;", "")
+            content = content.replaceAll("margin-top:0pt;", "")
+            content = content.replaceAll("margin-bottom:0pt;", "")
+            content = content.replaceAll("color: rgb(51, 51, 51);", "")
+
+            el.innerHTML = content
+
             formData.append(formAttributes[el.id], el.innerHTML);
             if (el.innerText.trim() == "") {
                 good = false
@@ -36,20 +67,18 @@ function submitInner() {
             }
         }
     }
+
+    const submissionID = Date.now() + "-" + Math.round(Math.random()*1000000)
+    formData.append("entry.639522065", submissionID);
     
     if (good) {
-        fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLSfQHvRNYlPBdWp3bI4FY5BlSGBovsynhuwuzMSwemeGoerrmg/formResponse', {
+       formSubmit = await fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLSfQHvRNYlPBdWp3bI4FY5BlSGBovsynhuwuzMSwemeGoerrmg/formResponse', {
             method: 'POST',
             body: formData,
             mode: 'no-cors' // Allows for cross-origin requests without redirect
         })
-        .then(response => {
-            response.text().then(x => window.location.href = "/formOut_success.html")
-            setTimeout(() => {window.location.href = "/formOut_timeout.html" }, 3000);
-        })
-        .catch(error => {
-            window.location.href = "/formOut_fail.html"
-        });
+
+        window.location.href = `/form_status.html?${submissionID}`
     } else {
         alert("Please fill out all required fields.")
     }
