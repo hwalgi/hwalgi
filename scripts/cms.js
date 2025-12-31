2d2aad1b07ab416ef85089b4c1b72a78b3436e7d
William Choi-Kim <will.choi.kim@gmail.com>
2025-12-31 04:10:04 -0600

    fix to cms

--- scripts/cms.js
+++ scripts/cms.js
@@ -5,10 +5,13 @@ fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vQNzHtt1-FLZgKBvCzwbrfHiY
     let titles = rows.map(x => x.slice(0, x.length - 2).join(",").trim())
     let tags = rows.map(x => x[x.length - 1].trim())
     for (let i = 0; i < titles.length; i++) {
+        if (titles.map(x => getSplat(x)).lastIndexOf(getSplat(titles[i])) != i) {
+            continue
+        }
         a = document.createElement("a")
         tit = titles[i].split("...").map(x => x.trim()).map(x => x.startsWith("\"") ? x.slice(1,) : x).map(x => x.endsWith("\"") ? x.slice(0, -1) : x)
         splat = getSplat(tit.join("\n"))
-        a.innerText = tit.join(" (").toUpperCase() + ")"
+        a.innerText = `${tit.join(" (").toUpperCase()}) / [${tags[i]}] / ${authors[i]}`
         document.getElementById("listOfPending").appendChild(a)
         a.href = `/edit.html?${splat}`
     }
