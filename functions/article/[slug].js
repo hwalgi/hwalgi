ff72b419e392ce5647c5268f269299b2288f7704
GitHub Action <action@github.com>
2026-01-01 06:13:52 +0000

    bump version for cache

--- functions/article/[slug].js
+++ functions/article/[slug].js
@@ -38,8 +38,8 @@ export async function onRequest(context) {
         <title>${tit[0].toLocaleLowerCase()} | Hwalgi</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="description" content="Hwalgi is host to award-winning, unflinching, uncompromised opinions." />
-        <link rel="stylesheet" type="text/css" href="/styles/style.css?v=1767247462" />
-        <link rel="stylesheet" type="text/css" href="/styles/article.css?v=1767247462" />
+        <link rel="stylesheet" type="text/css" href="/styles/style.css?v=1767248032" />
+        <link rel="stylesheet" type="text/css" href="/styles/article.css?v=1767248032" />
         <link rel="icon" href="/assets/images/icon.png">
         
         <!-- Open Graph / Facebook -->
@@ -92,11 +92,11 @@ export async function onRequest(context) {
             </div>
           </div>
         </div>
-        <script src="/scripts/sanitizeText.js?v=1767247462"></script>
-        <script src="/scripts/splat.js?v=1767247462"></script>
-        <script src="/scripts/comments.js?v=1767247462"></script>
-        <script src="/scripts/readUtils.js?v=1767247462"></script>
-        <script src="/scripts/read.js?v=1767247462"></script>
+        <script src="/scripts/sanitizeText.js?v=1767248032"></script>
+        <script src="/scripts/splat.js?v=1767248032"></script>
+        <script src="/scripts/comments.js?v=1767248032"></script>
+        <script src="/scripts/readUtils.js?v=1767248032"></script>
+        <script src="/scripts/read.js?v=1767248032"></script>
       </body>
       </html>
     `;
